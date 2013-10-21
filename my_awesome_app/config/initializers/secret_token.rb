a967b5315942136223abd19d6573d1cf1687e5f2
Sarah Gibson <sgibson2014@kellogg.northwestern.edu>
2013-10-20 19:32:54 -0500

    New app

---
+++ my_awesome_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyAwesomeApp::Application.config.secret_key_base = '2edf1203a6c87ced33516f13bb2cf9c7168bfaa2b6cf9de9a439887651729ec19e24811f54ebcd129fe42bc503c98d645b0a5687674b2548a98405b4d7a2271a'
